f32b5b97e66f4090131d266a70e71f50872c3019
SIVAPRASAD <avisdasarp03@gmail.com>
2026-08-20 00:10:57 +0530

    feat: update multithreading example using Thread start() in Multiple.java

--- 01-Core-Java/07-Threads/Multiple.java
+++ 01-Core-Java/07-Threads/Multiple.java
@@ -1,6 +1,36 @@
+class A extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Running");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B extends Thread {
+    public void run() { // Override run() instead of jog()
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Jogging");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 public class Multiple {
     public static void main(String[] args) {
-        // Multithreading example placeholder
-        System.out.println("Multithreading example");
+        A obj1 = new A();
+        B obj2 = new B();
+
+        // Call start() to execute concurrently on separate threads
+        obj1.start();
+        obj2.start();
     }
 }
